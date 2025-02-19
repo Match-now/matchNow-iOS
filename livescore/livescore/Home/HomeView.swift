74a840c8241991c2228e8f07f0b8963eea761130
morgan4563 <morganpark4563@gmail.com>
2025-02-20 05:39:05 +0900

    feat: 탭바 구현
    라이브탭, 커뮤니티탭, 뉴스탭, 관심경기탭, 더보기탭 화면전환 구현

---
+++ livescore/livescore/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  livescore
+//
+//  Created by hyunMac on 2/20/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("홈 뷰")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ livescore/livescore/Other/OtherView.swift
@@ -0,0 +1,18 @@
+//
+//  OtherView.swift
+//  livescore
+//
+//  Created by hyunMac on 2/20/25.
+//
+
+import SwiftUI
+
+struct OtherView: View {
+    var body: some View {
+        Text("더보기 뷰")
+    }
+}
+
+#Preview {
+    OtherView()
+}
