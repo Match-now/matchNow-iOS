746c6ec5a68971d5eab42e856c8d4abd516799fd
morgan4563 <morganpark4563@gmail.com>
2025-02-20 03:44:42 +0900

    프로젝트 초기 커밋

---
+++ livescore/livescore/livescoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  livescoreApp.swift
+//  livescore
+//
+//  Created by hyunMac on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct livescoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
